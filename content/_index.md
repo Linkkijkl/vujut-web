e9665efce19846fa3fbcc6931ceb1d356d01d661
Lari Nurkka <lari.nurkka@gmail.com>
2025-09-14 19:38:26 +0000

    set scroll offset based on navbar height

--- content/_index.md
+++ content/_index.md
@@ -1,5 +1,6 @@
 +++
 title = "Vuosijuhlat"
+id = "vuosijuhlat"
 +++
 
 Tietotekniikan ja koulutusteknologian opiskelijoiden ainejärjestö Linkki Jyväskylä ry juhlii 20-vuotisvuosijuhlaansa 20.3.2026. Juhla koostuu kolmesta osasta: kutsuvieraiden cocktail-tilaisuudesta, iltajuhlasta sekä seuraavan päivän silliaamiaisesta. 
--- static/js/main.js
+++ static/js/main.js
@@ -19,3 +19,13 @@ for (const sponsor of document.querySelectorAll('.sponsors img')) {
         }, DEBOUNCE_TIME);
     }, {once: true});
 }
+
+// Set scroll offset based on height of navbar
+window.addEventListener('load', () => {
+    // Get navbar height
+    let navbar = document.querySelector('.navbar');
+    let bottomLocation = navbar.getBoundingClientRect().bottom;
+
+    // Set scroll offset
+    document.styleSheets[0].insertRule(`:target { scroll-margin-top: ${bottomLocation}px; }`);
+});
